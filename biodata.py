63879ba1e662a07293c33db0b480014583b3b124
Naveenms48 <101813741+Naveenms48@users.noreply.github.com>
2023-09-15 09:05:00 +0530

    Add files via upload

---
+++ biodata.py
@@ -0,0 +1,14 @@
+print("NAVEENKUMAR")
+print("naveenms2411@gmail.com")
+print(20)
+print("Male")
+print(6369354326)
+print("24APR2003")
+print("Indian")
+print("Hindu")
+print("Unmarried")
+print("BCA")
+print("No.2/194,Radhakrishnan Street Madharpakkam")
+print("English","Tamil","Telugu")
+
+    
